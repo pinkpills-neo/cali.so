deeefb011184666978361219b6e772c6804dd247
pinkpills-neo <wangliyu.neo@bytedance.com>
2025-05-11 19:50:26 +0800

    修正基本描述

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -4,13 +4,13 @@ import { motion } from 'framer-motion'
 import Balancer from 'react-wrap-balancer'
 
 import { SparkleIcon, UserSecurityIcon } from '~/assets'
-import { PeekabooLink } from '~/components/links/PeekabooLink'
-import { SocialLink } from '~/components/links/SocialLink'
+// import { PeekabooLink } from '~/components/links/PeekabooLink'
+// import { SocialLink } from '~/components/links/SocialLink'
 
 function Developer() {
   return (
     <span className="group">
-      <span className="font-mono">&lt;</span>开发者
+      <span className="font-mono">&lt;</span>代码
       <span className="font-mono">/&gt;</span>
       <span className="invisible inline-flex text-zinc-300 before:content-['|'] group-hover:visible group-hover:animate-typing dark:text-zinc-500" />
     </span>
@@ -26,7 +26,7 @@ function Designer() {
         <span className="absolute -bottom-[3.5px] -left-[3.5px] size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
         <span className="absolute -right-[3.5px] -top-[3.5px] size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
       </span>
-      设计师
+      文章
     </span>
   )
 }
@@ -35,7 +35,7 @@ function OCD() {
   return (
     <span className="group inline-flex items-center">
       <SparkleIcon className="mr-1 inline-flex transform-gpu transition-transform duration-500 group-hover:rotate-180" />
-      <span>细节控</span>
+      <span>实用</span>
     </span>
   )
 }
@@ -44,7 +44,7 @@ function Founder() {
   return (
     <span className="group inline-flex items-center">
       <UserSecurityIcon className="mr-1 inline-flex group-hover:fill-zinc-600/20 dark:group-hover:fill-zinc-200/20" />
-      <span>创始人</span>
+      <span>创意</span>
     </span>
   )
 }
@@ -80,10 +80,11 @@ export function Headline() {
         }}
       >
         <Balancer>
-          我是 Cali，
-          <PeekabooLink href="https://zolplay.com">佐玩</PeekabooLink>
-          创始人，目前带领着佐玩致力于创造一个充满创造力的工作环境，同时鼓励团队创造影响世界的产品。
-          我热爱开发，设计，创新，享受生活，以及在未知领域中探索。
+          我是 Neo, 这是我写字和实验的地方
+        </Balancer>
+        <br />
+        <Balancer>
+          AI时代来临了, 我要尝试一下
         </Balancer>
       </motion.p>
       <motion.div
@@ -98,7 +99,7 @@ export function Headline() {
           delay: 0.25,
         }}
       >
-        <SocialLink
+        {/* <SocialLink
           href="https://cali.so/twitter"
           aria-label="我的推特"
           platform="twitter"
@@ -128,7 +129,7 @@ export function Headline() {
           href="mailto:hi@cali.so"
           aria-label="我的邮箱"
           platform="mail"
-        />
+        /> */}
       </motion.div>
     </div>
   )

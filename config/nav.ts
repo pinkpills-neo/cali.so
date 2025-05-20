c9e9254c9ee59a3ff1cb830d17c4797aa3fee146
pinkpills-neo <wangliyu.neo@bytedance.com>
2025-05-20 22:04:23 +0800

    update nav

--- config/nav.ts
+++ config/nav.ts
@@ -1,6 +1,7 @@
 export const navigationItems = [
   { href: '/', text: '首页' },
   { href: '/blog', text: '博客' },
+  { href: '/personal_workspace', text: '工作台' },
   { href: '/projects', text: '项目' },
   { href: '/guestbook', text: '留言墙' },
   // { href: '/ama', text: 'AMA 咨询' },
